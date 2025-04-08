e54f49d0a80d7378ec65627a87dcd2f1a0b125a4
Matt MacDermott <m.macdermott@saifh.org>
2025-04-08 11:03:48 -0400

    finish experiments

--- slurm_script.sh
+++ slurm_script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=train_ce
+#SBATCH --job-name=predict_ce_test
 #SBATCH --output=logs/%j.out
 #SBATCH --error=logs/%j.err
 #SBATCH --time=3:00:00
 #SBATCH --mem=32G
-#SBATCH --partition=short-unkillable
-#SBATCH --gres=gpu:a100l:4
+#SBATCH --partition=unkillable
+#SBATCH --gres=gpu:a100l:1
 #SBATCH --cpus-per-task=4
 
 
@@ -27,25 +27,25 @@ module --quiet load anaconda/3
 #     --num_gpus 4 \
 #     --final_forward_batch_size 2
 
-python train_discrepancy_pred.py \
-    --model_name "Qwen/Qwen2.5-3B-Instruct" \
-    --json_path "results/test_mmlu_discrepancy.json" \
-    --discrepancy "ce" \
-    --num_epochs 25 \
-    --batch_size 8 \
-    --learning_rate 1e-5 \
-    --accumulate_steps 2 \
-    --output_dir "$SCRATCH/mc_distill" \
-    --save_every 1 \
+# python train_discrepancy_pred.py \
+#     --model_name "Qwen/Qwen2.5-3B-Instruct" \
+#     --json_path "results/test_mmlu_discrepancy.json" \
+#     --discrepancy "ce" \
+#     --num_epochs 25 \
+#     --batch_size 8 \
+#     --learning_rate 1e-5 \
+#     --accumulate_steps 2 \
+#     --output_dir "$SCRATCH/mc_distill" \
+#     --save_every 1 \
 #     --debug \
 #     --debug_samples 8
 
-# python predict_discrepancy.py \
-#     --discrepancy "kl_via_entropy" \
-#     --eval_split "validation" \
-#     --json_path "results/" \
-#     --checkpoint "$SCRATCH/mc_distill/" \
-#     --model_name "Qwen/Qwen2.5-3B-Instruct" \
-#     --output_path "results/"
+python predict_discrepancy.py \
+    --discrepancy "ce" \
+    --eval_split "test" \
+    --json_path "results/" \
+    --checkpoint "$SCRATCH/mc_distill/07-04/" \
+    --model_name "Qwen/Qwen2.5-3B-Instruct" \
+    --output_path "results/"
 
 echo "Finished running"
